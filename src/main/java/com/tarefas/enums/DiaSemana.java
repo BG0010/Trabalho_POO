fc6c3c0ed2f3e03b8ff84a7ebff962fb4c34295d
Henrique Soares <hiiski.lima@gmail.com>
2026-06-01 22:28:35 -0300

    feat: [Etapa 2] Domínio e Modelagem de Dados

---
+++ src/main/java/com/tarefas/enums/DiaSemana.java
@@ -0,0 +1,15 @@
+package com.tarefas.enums;
+
+/**
+ * Enum que representa os dias da semana.
+ */
+public enum DiaSemana {
+
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO,
+    DOMINGO
+}
